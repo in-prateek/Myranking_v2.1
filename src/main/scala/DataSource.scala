f16e85022e04d07936fed67482677a20be6c21ce
Maitreya <think1idea@gmail.com>
2018-06-26 17:41:48 +0530

    ss

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -12,7 +12,7 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 
 import grizzled.slf4j.Logger
-case class DataSourceParams(appName: String, prop1 : String) extends Params
+case class DataSourceParams(appName: String, property : Array[String]) extends Params
 
 class DataSource(val dsp: DataSourceParams)
   extends PDataSource[TrainingData,
